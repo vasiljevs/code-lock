fcf2edc036b8f60026e0df3846f4e1d84f7f2302
Arturs Vasiljevs <freelancerarthur@gmail.com>
2018-05-03 18:48:53 +0300

    Add - most of the functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const code = document.querySelector('.lock-code'),
+copyBtn = document.querySelector('.lock-copy'),
+genBtn = document.querySelector('.lock-generate'),
+history = document.querySelector('.code-history');
+
+const randNum = () => (Math.floor(Math.random() * 10000) + 10000).toString().substring(1);
+
+genBtn.addEventListener('click', (e) => {
+  code.textContent = randNum();
+
+  let numArr; 
+
+  if (localStorage.getItem('codes') === null) {
+    numArr = [];
+  } else {
+    numArr = JSON.parse(localStorage.getItem('codes'));
+  }
+
+  numArr.push(code.textContent);
+  localStorage.setItem('codes', JSON.stringify(numArr.slice(-5)));
+
+  e.preventDefault();
+});
+
+const listArr = JSON.parse(localStorage.getItem('codes'));
+
+if (listArr != null) {
+  listArr.reverse().forEach((num) => {
+    const li = document.createElement('li');
+    const data = document.createTextNode(num);
+    li.appendChild(data);
+    history.appendChild(li);
+  });
+}
+
